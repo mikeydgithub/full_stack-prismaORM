cf5385215d9a69fb9e73e958d2cd976f94761d7e
Mike <ampgzo2@gmail.com>
2022-07-21 16:09:03 -0400

    add list components to index.tsx

--- grocery-list/src/components/index.tsx
+++ grocery-list/src/components/index.tsx
@@ -61,3 +61,35 @@ export const CardHeader: NextPage<CardHeaderProps> = ({
         </div>
     );
 };
+
+
+// components for list
+
+export const List: NextPage<CardProps> = ({ children }) => {
+    return <div className="overflow-y-auto h-72">
+        {children}
+    </div>
+};
+
+// interface list item props
+interface ListItemProps {
+    item: GroceryList;
+    onUpdate?: (item: GroceryList) => void;
+}
+
+// list item component
+const ListItemComponent: NextPage<ListItemProps> = ({ item, onUpdate}) => {
+    return (
+        <div className="h-12 border-b flex items-center justify-start px-3">
+            <input
+                type="checkbox"
+                className="w-4 h-4 border-gray-300 rounded mr-4"
+                defaultChecked={item.checked as boolean}
+                onChange={() => onUpdate?.(item)}
+            />
+            <h2 className="text-gray-600 tracking-wide text-sm">{item.title}</h2>
+        </div>
+    );
+};
+
+export const ListItem = memo(ListItemComponent);
