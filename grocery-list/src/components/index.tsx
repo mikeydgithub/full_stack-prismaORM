8faad0089fc427c8dbe15e732771f46b39195497
Mike <ampgzo2@gmail.com>
2022-07-21 13:43:31 -0400

    create cards in index.tsx

---
+++ grocery-list/src/components/index.tsx
@@ -0,0 +1,63 @@
+import React, { memo } from "react";
+import type { NextPage } from "next";
+import { GroceryList } from "@prisma/client";
+
+// create the front end
+// start with a cards container
+
+// interface card prop
+interface CardProps {
+    children: React.ReactNode;
+}
+
+
+// single card
+export const Card: NextPage<CardProps> = ({ children }) => {
+    return (
+        <div className="h-screen flex flex-col justify-center items-center bg-slate-100">
+            {children}
+        </div>
+    );
+};
+
+// card content
+export const CardContent: NextPage<CardProps> = ({ children }) => {
+    return (
+        <div className="bg-white w-5/6 md:w-4/6 lg:w-3/6 xl:w-2/6 rounded-lg drop-shadow-md">
+            {children}
+        </div>
+    );
+};
+
+// interface card header prop
+interface CardHeaderProps {
+    title: string;
+    listLength: number;
+    clearAllFn?: () => void;
+}
+
+export const CardHeader: NextPage<CardHeaderProps> = ({
+    title,
+    listLength,
+    clearAllFn,
+}) => {
+    return (
+        <div className="flex flex-row items-center justify-between p-3 border-b border-slate-200">
+            <div className="flex flex-row items-center justify-between">
+                <h1 className="text-base font-medium tracking-wide text-gray-900 mr-2">
+                    {title}
+                </h1>
+                <span className="h-5 w-5 bg-blue-200 text-blue-600 flex items-center justify-center rounded-full text-xs">
+                    {listLength}
+                </span>
+            </div>
+            <button
+            className="text-sm font-medium text-gray-600 underline"
+            type="button"
+            onClick={clearAllFn}
+            >
+                Clear All
+            </button>
+        </div>
+    );
+};
--- grocery-list/src/utils/trpc.ts
+++ grocery-list/src/utils/trpc.ts
@@ -2,3 +2,6 @@ import type { ServerRouter } from "@/server/router";
 import { createReactQueryHooks } from "@trpc/react";
 
 export const trpc = createReactQueryHooks<ServerRouter>();
+
+// tRPC hook, to which we add the data type of our router as generic
+// on the createreactqueryhooks() function so we can make API calls
